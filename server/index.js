48499e5edd0d69411d31b677c081cf1a272ac5e0
arjun-saseendran <arjunsaseendran@icloud.com>
2025-06-25 20:08:19 +0530

    add createTodo

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import dotenv from 'dotenv'
-import {dbConnect} from './db/dbConnection.js'
+import express from "express";
+import dotenv from "dotenv";
+import { dbConnect } from "./db/dbConnection.js";
+import { todosRouter } from "./routes/todoRoutes.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
+
 app.use(express.json());
+app.use("/", todosRouter);
 
 await dbConnect();
 
-app.listen(PORT, ()=> console.log(`Server running on ${PORT}`));
-
-
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
